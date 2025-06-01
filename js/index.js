1f08986e6f404008617491fd6de074ff1bc4cae0
eleo-n <elizm0rri@gmail.com>
2025-06-01 11:08:56 +0300

    Add video carousel

--- js/index.js
+++ js/index.js
@@ -1,11 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Initialize carousels
+  // Initialize image carousel
   bulmaCarousel.attach('#results-carousel', {
     slidesToScroll: 1,
     slidesToShow: 1,
     loop: true,
     autoplay: true,
     autoplaySpeed: 5000,
+    pauseOnHover: true
+  });
+
+  // Initialize video carousel
+  bulmaCarousel.attach('#video-carousel', {
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    loop: true,
+    autoplay: false, // Don't autoplay videos
     pauseOnHover: true
   });
 });
